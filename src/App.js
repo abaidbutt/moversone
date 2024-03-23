7a10735872e856514c8f71327a601b0dfb17bdbb
Abaid Ullah <49604057+abaidbutt@users.noreply.github.com>
2024-03-23 10:46:30 +0500

    Update App.js for firestore credentials

--- src/App.js
+++ src/App.js
@@ -17,17 +17,9 @@ import {
   onSnapshot,
 } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
-
+import {firebaseConfig} from './firebaseConfig';
 import { useNavigate } from "react-router-dom";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCr9Y1YZca5eoEMI69o1VcMpw74PEYH3ww",
-  authDomain: "bussticketapp.firebaseapp.com",
-  projectId: "bussticketapp",
-  storageBucket: "bussticketapp.appspot.com",
-  messagingSenderId: "79830776119",
-  appId: "1:79830776119:web:feace03101cd48abac82b2",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
