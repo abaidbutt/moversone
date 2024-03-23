826a0553dacb30746dfa76604d791775140e3f0e
Abaid Ullah <49604057+abaidbutt@users.noreply.github.com>
2024-03-23 10:46:57 +0500

    Create firebaseConfig.js private

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCr9Y1YZca5eoEMI69o1VcMpw74PEYH3ww",
+  authDomain: "bussticketapp.firebaseapp.com",
+  projectId: "bussticketapp",
+  storageBucket: "bussticketapp.appspot.com",
+  messagingSenderId: "79830776119",
+  appId: "1:79830776119:web:feace03101cd48abac82b2",
+};
